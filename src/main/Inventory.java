1140ba91c13b557fedae3ac655674c81dc8206bd
Taylor Meyer <taylor.ross.meyer@gmail.com>
2020-04-25 20:12:10 -0700

    Create basic item & inventory classes

---
+++ src/main/Inventory.java
@@ -0,0 +1,64 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Inventory {
+
+    private ArrayList<Item> items;
+    private int gold;
+
+    private int maxSize = 20;
+
+    public Inventory(int gold) {
+        this.items = new ArrayList();
+        this.gold = gold;
+    }
+
+    public void addItem(Item item) {
+        if(this.items.size() < maxSize)
+            this.items.add(item);
+    }
+
+    public void addGold(int gold) {
+        this.gold += gold;
+    }
+
+    public void subtractGold(int gold) {
+        if (this.gold - gold < 0)
+            this.gold = 0;
+        else
+            this.gold -= gold;
+    }
+
+    public boolean hasEnoughGold(int gold) {
+        if (this.gold > gold)
+            return true;
+        else
+            return false;
+    }
+
+    /* Getters & Setters */
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMax_size(int max_size) {
+        this.maxSize = max_size;
+    }
+}
---
+++ src/main/Item.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Item {
+
+    private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    /* Getters & Setters */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
