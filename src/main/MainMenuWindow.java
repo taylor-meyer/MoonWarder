45df2ab02dd8b33cb15fdef2c222a93898620a5a
taylor-meyer <taylor.ross.meyer@gmail.com>
2020-05-06 23:17:19 -0700

    Implement character loading through GSON & JSON

--- src/main/MainMenuWindow.java
+++ src/main/MainMenuWindow.java
@@ -1,5 +1,6 @@
 package main;
 
+import com.google.gson.Gson;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -8,6 +9,11 @@ import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class MainMenuWindow extends Stage {
 
@@ -33,11 +39,36 @@ public class MainMenuWindow extends Stage {
             }
         });
 
+        // Load Button
+        Button btn_load = new Button("Load");
+        btn_load.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String filePath = new JFileChooser().getFileSystemView().getDefaultDirectory().toString() + "\\"
+                        + "moonwarder_character.json";
+
+                BufferedReader bufferedReader = null;
+                try {
+                    bufferedReader = new BufferedReader(new FileReader(filePath));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                if (bufferedReader != null) {
+                    Gson gson = new Gson();
+                    Character C = gson.fromJson(bufferedReader, Character.class);
+                    PlayWindow pw = new PlayWindow(C);
+                    pw.show();
+                    close();
+                }
+            }
+        });
+
         Text text_title = new Text("MoonWarder");
         text_title.setStyle("-fx-font-size: 20");
 
         VBox vbox = new VBox();
-        vbox.getChildren().addAll(text_title, btn_newGame, btn_exit);
+        vbox.getChildren().addAll(text_title, btn_newGame, btn_load, btn_exit);
         vbox.setAlignment(Pos.CENTER);
 
         this.setScene(new Scene(vbox, 300,300));
