b4c5e26364c8e3afea3b497cd2a8132d0d28c07e
Taylor Meyer <taylor.ross.meyer@gmail.com>
2020-08-01 20:19:52 -0700

    Create Entity class

---
+++ src/main/Entity.java
@@ -0,0 +1,41 @@
+package main;
+
+public class Entity {
+
+    /* TODO */
+    // Rework structure such that character and future npcs/buildings etc. are
+    // inherit this class
+    private String name;
+    private int Xpos;
+    private int Ypos;
+
+    public Entity(String name, int xpos, int ypos) {
+        this.name = name;
+        Xpos = xpos;
+        Ypos = ypos;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getXpos() {
+        return Xpos;
+    }
+
+    public void setXpos(int xpos) {
+        Xpos = xpos;
+    }
+
+    public int getYpos() {
+        return Ypos;
+    }
+
+    public void setYpos(int ypos) {
+        Ypos = ypos;
+    }
+}
