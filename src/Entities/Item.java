248350f7b4c2a097e33af0dbf97b76e65e7b123f
taylor-meyer <taylor.ross.meyer@gmail.com>
2020-08-21 18:58:52 -0700

    Change Item class to inherit Entity

---
+++ src/Entities/Item.java
@@ -0,0 +1,10 @@
+package Entities;
+
+public class Item extends Entity {
+
+    /* TODO */
+    // Item stats, quest item, equippable items, etc
+    public Item(String name, int xpos, int ypos) {
+        super(name, xpos, ypos);
+    }
+}
